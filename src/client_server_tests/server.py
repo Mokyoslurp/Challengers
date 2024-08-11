398e351f15b8cce0e8691d5ae32b6825943ae2fd
Marien Giordani <marien.giordani@student.isae-supaero.fr>
2024-08-12 00:12:02 +0200

    Added methods to read and format players positions, and send them to the other players.

--- src/client_server_tests/server.py
+++ src/client_server_tests/server.py
@@ -7,23 +7,42 @@ SERVER = "192.168.0.102"
 PORT = 5050
 
 
-def threaded_client(connection: socket.socket):
-    connection.send(str.encode("Connected"))
+positions = [(0, 0), (100, 100)]
+
+
+def read_position(string: str):
+    string = string.split(",")
+    return int(string[0]), int(string[1])
+
+
+def make_position(position: tuple[int, int]):
+    return str(position[0]) + "," + str(position[1])
+
+
+def threaded_client(connection: socket.socket, player_id: int):
+    connection.send(
+        str.encode(
+            make_position(positions[player_id]),
+        )
+    )
     reply = ""
     while True:
         try:
             # Argument is amount of information you want to receive (bits)
-            data = connection.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_position(connection.recv(2048).decode())
+            positions[player_id] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
+                # Replies the other player's position
+                reply = positions[abs(player_id - 1)]
+
                 print("Received: ", reply)
                 print("Sending: ", reply)
 
-            connection.sendall(str.encode(reply))
+            connection.sendall(str.encode(make_position(reply)))
         except:  # noqa: E722
             break
 
@@ -39,6 +58,8 @@ if __name__ == "__main__":
     except socket.error as e:
         print(e)
 
+    current_players = 0
+
     # Argument is the number of client that can connect
     s.listen(2)
     print("Waiting for connection, server started!")
@@ -47,4 +68,5 @@ if __name__ == "__main__":
         connection, address = s.accept()
         print("Connected to: ", address)
 
-        start_new_thread(threaded_client, (connection,))
+        start_new_thread(threaded_client, (connection, current_players))
+        current_players += 1
