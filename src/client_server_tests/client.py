9ce570c04111573600df09eec5a168a55ecdd484
Marien Giordani <marien.giordani@student.isae-supaero.fr>
2024-08-12 00:09:36 +0200

    Added a way for a client to actually see the other client position, and send its information to the server for the other client to read.

--- src/client_server_tests/client.py
+++ src/client_server_tests/client.py
@@ -1,5 +1,8 @@
 import pygame
 
+from network import Network
+from server import read_position, make_position
+
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
@@ -32,30 +35,57 @@ class Player:
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(player: Player):
+def redrawWindow(player1: Player, player2: Player):
     win.fill((255, 255, 255))
-    player.draw(win)
+    player1.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
     run = True
+    network = Network()
+    start_position = read_position(network.get_position())
 
-    player = Player(50, 50, 100, 100, (0, 255, 0))
+    player1 = Player(
+        start_position[0],
+        start_position[1],
+        100,
+        100,
+        (0, 255, 0),
+    )
+    player2 = Player(
+        0,
+        0,
+        100,
+        100,
+        (0, 255, 0),
+    )
+
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
+
+        player2_position = read_position(network.send(make_position((player1.x, player1.y))))
+        player2.x = player2_position[0]
+        player2.y = player2_position[1]
+        player2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
-        player.move()
-        redrawWindow(player)
+        player1.move()
+        redrawWindow(player1, player2)
 
 
-main()
+if __name__ == "__main__":
+    main()
