1ff26b3dea64947bf4445595f11a8460f75a67ff
Marien Giordani <marien.giordani@student.isae-supaero.fr>
2024-08-13 00:46:05 +0200

    Created basic class to represent a card.

---
+++ src/challengers/game/card.py
@@ -0,0 +1,45 @@
+from typing import Self
+from enum import Enum
+
+
+class Set(Enum):
+    CITY = 0
+    CASTLE = 1
+    FUNFAIR = 2
+    OUTER_SPACE = 3
+    FILM_STUDIO = 4
+    HAUNTED_HOUSE = 5
+    SHIPWRECK = 6
+
+
+class Level(Enum):
+    A = 0
+    B = 1
+    C = 2
+
+
+class Card:
+    cards: list[Self] = []
+
+    def __init__(self, id: int, name: str, set: Set, level: Level, is_starter: bool = False):
+        self.id = id
+        self.name = name
+        self.set = set
+        self.level = level
+        self.is_starter = is_starter
+
+    @classmethod
+    def create(
+        cls,
+        id: int,
+        name: str,
+        set: Set,
+        level: Level,
+        is_starter: bool = False,
+        amount: int = 1,
+    ):
+        cards = []
+        for _ in range(amount):
+            cards.append(Card(id, name, set, level, is_starter))
+        cls.cards += cards
+        return cards
