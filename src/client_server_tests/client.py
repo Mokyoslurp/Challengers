9ae7993f3e5eb53ac0cb4eb13093fa0b56c74dd9
Marien Giordani <giordanimarien@gmail.com>
2024-08-12 10:07:10 +0200

    Player class moved to a seperate file.

--- src/client_server_tests/client.py
+++ src/client_server_tests/client.py
@@ -1,6 +1,7 @@
 import pygame
 
 from network import Network
+from player import Player
 from server import read_position, make_position
 
 width = 500
@@ -9,36 +10,6 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 clientNumber = 0
-
-
-class Player:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.height = height
-        self.width = width
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 3
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
 
 
 def redrawWindow(player1: Player, player2: Player):
