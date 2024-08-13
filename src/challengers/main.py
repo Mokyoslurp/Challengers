bf8cce97428ba5e5f7dc635b00dd4459e17afe6e
Marien Giordani <giordanimarien@gmail.com>
2024-08-13 13:11:25 +0200

    Added test code to test a round between two players.

--- src/challengers/main.py
+++ src/challengers/main.py
@@ -4,14 +4,39 @@ from game import (
     Set,
     Tray,
     Player,
+    Park,
 )
 
 
 def load_cards():
-    cards1 = Card.create(0, "Test1", Set.CITY, Level.A, power=3, amount=4)
-    cards2 = Card.create(1, "Test2", Set.HAUNTED_HOUSE, Level.A, power=5, amount=2)
+    cards1 = Card.create(1, "Test1", Set.CITY, Level.A, power=3, amount=4)
+    cards2 = Card.create(2, "Test2", Set.HAUNTED_HOUSE, Level.A, power=5, amount=2)
+    cards3 = Card.create(3, "Test3", Set.OUTER_SPACE, Level.A, power=4, amount=3)
+    cards4 = Card.create(4, "Test4", Set.SHIPWRECK, Level.A, power=1, amount=5)
+    cards5 = Card.create(5, "Test5", Set.CASTLE, Level.A, power=2, amount=2)
+    cards6 = Card.create(6, "Test6", Set.FILM_STUDIO, Level.A, power=2, amount=1)
+    cards7 = Card.create(7, "Test7", Set.FUNFAIR, Level.A, power=3, amount=1)
+    cards8 = Card.create(8, "Test8", Set.SHIPWRECK, Level.A, power=4, amount=2)
+    # cards9 = Card.create(9, "Test8", Set.CITY, Level.A, power=4, amount=2)
+    # cards10 = Card.create(10, "Test8", Set.CASTLE, Level.A, power=2, amount=5)
+    # cards11 = Card.create(11, "Test8", Set.OUTER_SPACE, Level.A, power=2, amount=3)
+    # cards12 = Card.create(12, "Test8", Set.OUTER_SPACE, Level.A, power=3, amount=2)
+    # cards13 = Card.create(13, "Test8", Set.FILM_STUDIO, Level.A, power=5, amount=1)
+    # cards14 = Card.create(14, "Test8", Set.FUNFAIR, Level.A, power=6, amount=1)
     print(cards1[0])
     print(cards2[0])
+    print(cards3[0])
+    print(cards4[0])
+    print(cards5[0])
+    print(cards6[0])
+    print(cards7[0])
+    print(cards8[0])
+    # print(cards9[0])
+    # print(cards10[0])
+    # print(cards11[0])
+    # print(cards12[0])
+    # print(cards13[0])
+    # print(cards14[0])
 
 
 def initialize_trays():
@@ -36,12 +61,21 @@ if __name__ == "__main__":
     print(player1)
     print(player2)
 
-    for _ in range(3):
+    for _ in range(10):
         player1.draw_card(trays[Level.A])
         player2.draw_card(trays[Level.A])
 
     player1.shuffle_deck()
     player2.shuffle_deck()
+
+    print(player1)
+    print(player2)
+
+    park = Park(0)
+    park.assign_players(player1, player2)
+
+    print("\n\nGame started\n")
+    winner = park.play_game()
 
     print(player1)
     print(player2)
