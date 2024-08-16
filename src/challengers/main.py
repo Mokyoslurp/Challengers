20f5e88822b9ec1b3ccdcac7e4fcba15b69f344a
Marien Giordani <marien.giordani@student.isae-supaero.fr>
2024-08-16 11:34:04 +0200

    Use of card serializer to load cards from json file.

--- src/challengers/main.py
+++ src/challengers/main.py
@@ -1,11 +1,19 @@
-from .game import (
-    Card,
+from pathlib import Path
+
+
+from challengers.game import (
     Level,
-    Set,
     Player,
     Tournament,
     Trophy,
+    CardSerializer,
 )
+
+
+GAME_DATA_PATH = Path(__file__).parent / "game" / "data"
+CARD_DATA_FILE = "cards.json"
+
+CARD_DATA_FILE_PATH = GAME_DATA_PATH / CARD_DATA_FILE
 
 # Key is the round, and the list following is all the possible fans for a trophy of this round.
 TROPHIES: dict[int, list[int]] = {
@@ -38,40 +46,8 @@ def load_trophies(tournament: Tournament):
     Trophy.shuffle_trophies()
 
 
-# 48 cards =  6*8
-def load_cards():
-    cards1 = Card.create(1, "Test1", Set.CITY, Level.A, power=3, amount=4)
-    cards2 = Card.create(2, "Test2", Set.HAUNTED_HOUSE, Level.A, power=5, amount=4)
-    cards3 = Card.create(3, "Test3", Set.OUTER_SPACE, Level.A, power=4, amount=3)
-    cards4 = Card.create(4, "Test4", Set.SHIPWRECK, Level.A, power=1, amount=5)
-    cards5 = Card.create(5, "Test5", Set.CASTLE, Level.A, power=2, amount=2)
-    cards6 = Card.create(6, "Test6", Set.FILM_STUDIO, Level.A, power=2, amount=3)
-    cards7 = Card.create(7, "Test7", Set.FUNFAIR, Level.A, power=3, amount=3)
-    cards8 = Card.create(8, "Test8", Set.SHIPWRECK, Level.A, power=4, amount=4)
-    cards9 = Card.create(9, "Test8", Set.CITY, Level.A, power=4, amount=2)
-    cards10 = Card.create(10, "Test8", Set.CASTLE, Level.A, power=2, amount=7)
-    cards11 = Card.create(11, "Test8", Set.OUTER_SPACE, Level.A, power=2, amount=3)
-    cards12 = Card.create(12, "Test8", Set.OUTER_SPACE, Level.A, power=3, amount=2)
-    cards13 = Card.create(13, "Test8", Set.FILM_STUDIO, Level.A, power=5, amount=3)
-    cards14 = Card.create(14, "Test8", Set.FUNFAIR, Level.A, power=6, amount=3)
-    print(cards1[0])
-    print(cards2[0])
-    print(cards3[0])
-    print(cards4[0])
-    print(cards5[0])
-    print(cards6[0])
-    print(cards7[0])
-    print(cards8[0])
-    print(cards9[0])
-    print(cards10[0])
-    print(cards11[0])
-    print(cards12[0])
-    print(cards13[0])
-    print(cards14[0])
-
-
 if __name__ == "__main__":
-    load_cards()
+    CardSerializer.load_cards_from_file(CARD_DATA_FILE_PATH)
 
     tournament = Tournament(8)
 
