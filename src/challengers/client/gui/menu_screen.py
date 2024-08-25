1fbc9c21dd222b507439ffd7a0424d81e7db6691
Marien Giordani <giordanimarien@gmail.com>
2024-08-25 20:11:47 +0200

    Added buttons to menu screen to communicate with server.

--- src/challengers/client/gui/menu_screen.py
+++ src/challengers/client/gui/menu_screen.py
@@ -1,9 +1,21 @@
+import pygame
+
+
 from .button import Button
 
 
 class MenuScreen:
     def __init__(self):
-        self.enter_server_button = Button(10, 10, "Enter server")
+        self.enter_server_button: Button = Button(10, 10, "Enter server")
+        self.leave_server_button: Button = Button(300, 10, "leave server")
+        self.test_button: Button = Button(10, 300, "Test")
 
-    def draw(self):
-        self.enter_server_button.draw()
+        self.buttons: list[Button] = [
+            self.enter_server_button,
+            self.leave_server_button,
+            self.test_button,
+        ]
+
+    def draw(self, window: pygame.Surface):
+        for button in self.buttons:
+            button.draw(window)
