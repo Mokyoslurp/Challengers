23bddfc44f884f5525a5bfb5ab5b520b1f9a4bf6
Marien Giordani <giordanimarien@gmail.com>
2024-08-25 10:04:54 +0200

    Moved Network class into Client for simplicity.

--- src/challengers/client/client.py
+++ src/challengers/client/client.py
@@ -1,5 +1,14 @@
 import pygame
 
+import socket
+import pickle
+
+from challengers.server.server import SERVER_IP, PORT
+from challengers.client.gui import MenuScreen
+
+
+BUFSIZE = 4096
+
 
 class Client:
     def __init__(self):
@@ -13,8 +22,33 @@ class Client:
         )
         pygame.display.set_caption("Client")
 
+        self.menu_screen = MenuScreen()
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (SERVER_IP, PORT)
+
+    def connect(self):
+        try:
+            self.socket.connect(self.server_address)
+
+            # Here we use decode() since the first message received is the player id
+            return self.socket.recv(BUFSIZE).decode()
+
+        except:  # noqa: E722
+            pass
+
+    def send(self, data):
+        try:
+            # serialization with str.encode() and str.decode() can be done for strings instead of pickle
+            self.socket.send(str.encode(data))
+            return pickle.loads(self.socket.recv(BUFSIZE))
+        except socket.error as e:
+            print(e)
+
     def draw(self):
         self.window.fill((128, 128, 128))
+
+        self.menu_screen.draw()
 
         pygame.display.update()
 
