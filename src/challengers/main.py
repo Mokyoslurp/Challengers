f4bcd3476d5c40c787724a25fdf64a05355b3bf5
Marien Giordani <giordanimarien@gmail.com>
2024-08-26 00:00:40 +0200

    Reapply "Reduced window size."
    This reverts commit a512f637606373bc618b44ec08a3c0bc19c006d4.

--- src/challengers/main.py
+++ src/challengers/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 
-from challengers.game import Player, Tournament
+from challengers.game import Tournament
+from challengers.server import Server
 
 
 GAME_DATA_PATH = Path(__file__).parent / "game" / "data"
@@ -11,26 +12,14 @@ TROPHY_DATA_FILE = "trophies.json"
 CARD_DATA_FILE_PATH = GAME_DATA_PATH / CARD_DATA_FILE
 TROPHY_DATA_FILE_PATH = GAME_DATA_PATH / TROPHY_DATA_FILE
 
-PLAYERS = [
-    Player(1, "P1"),
-    Player(2, "P2"),
-    Player(3, "P3"),
-    Player(4, "P4"),
-    Player(5, "P5"),
-    Player(6, "P6"),
-    Player(7, "P7"),
-    Player(8, "P8"),
-]
 
 if __name__ == "__main__":
-    tournament = Tournament(8)
+    server = Server(Tournament(8))
 
-    tournament.load_game_cards(CARD_DATA_FILE_PATH)
-    tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
+    server.tournament.load_game_cards(CARD_DATA_FILE_PATH)
+    server.tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
 
-    for player in PLAYERS:
-        tournament.set_new_player(player)
+    server.run()
+    # winner = tournament.play()
 
-    winner = tournament.play()
-
-    tournament.print_scores()
+    # tournament.print_scores()
