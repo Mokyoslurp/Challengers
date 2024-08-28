77f4719a27ec8471ad93dfe215c91bebb0292eaf
Marien Giordani <giordanimarien@gmail.com>
2024-08-28 21:42:00 +0200

    Added constants to use for GUI, such as colors.

---
+++ src/challengers/client/gui/util/__init__.py
@@ -0,0 +1,4 @@
+# ruff: noqa: F401
+
+from .text_rect import render_text_rect
+from .constants import WHITE, BLACK
---
+++ src/challengers/client/gui/util/constants.py
@@ -0,0 +1,5 @@
+import pygame
+
+
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
