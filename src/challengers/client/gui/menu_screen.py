6fcd9f86d8c0fc0ccdcb9e97be3ecbaa8a30a058
Marien Giordani <giordanimarien@gmail.com>
2024-09-04 13:54:33 +0200

    Added test buttons for testing GUI park functionalities.

--- src/challengers/client/gui/menu_screen.py
+++ src/challengers/client/gui/menu_screen.py
@@ -11,11 +11,23 @@ class MenuScreen:
         self.test_button: Button = Button(10, 300, "Test")
         self.launch_button: Button = Button(300, 300, "Launch")
 
+        self.add_bench_1_button: Button = Button(10, 500, "Bench 1")
+        self.add_bench_2_button: Button = Button(300, 500, "Bench 2")
+        self.add_played_1_button: Button = Button(10, 700, "Play 1")
+        self.add_played_2_button: Button = Button(300, 700, "Play 2")
+
+        self.reset_button: Button = Button(150, 400, "Reset")
+
         self.buttons: list[Button] = [
             self.enter_server_button,
             self.leave_server_button,
             self.test_button,
             self.launch_button,
+            self.add_bench_1_button,
+            self.add_bench_2_button,
+            self.add_played_1_button,
+            self.add_played_2_button,
+            self.reset_button,
         ]
 
     def draw(self, window: pygame.Surface):
