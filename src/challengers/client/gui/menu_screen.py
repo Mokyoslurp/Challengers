b7e44badb2e23d0a1820172eaab7abcce2f0854f
Marien Giordani <giordanimarien@gmail.com>
2024-09-06 19:08:54 +0200

    Made basic menu to get players ready.

--- src/challengers/client/gui/menu_screen.py
+++ src/challengers/client/gui/menu_screen.py
@@ -1,33 +1,21 @@
 import pygame
 
 
-from .components.button import Button
+from .components import Button, TextField
 
 
 class MenuScreen:
     def __init__(self):
         self.enter_server_button: Button = Button(10, 10, "Enter server")
         self.leave_server_button: Button = Button(300, 10, "leave server")
-        self.test_button: Button = Button(10, 300, "Test")
-        self.launch_button: Button = Button(300, 300, "Launch")
-
-        self.add_bench_1_button: Button = Button(10, 500, "Bench 1")
-        self.add_bench_2_button: Button = Button(300, 500, "Bench 2")
-        self.add_played_1_button: Button = Button(10, 700, "Play 1")
-        self.add_played_2_button: Button = Button(300, 700, "Play 2")
-
-        self.reset_button: Button = Button(150, 400, "Reset")
+        self.ready_button: Button = Button(10, 300, "Ready")
+        self.player_name_text_field: TextField = TextField(300, 300, "Type your name here")
 
         self.buttons: list[Button] = [
             self.enter_server_button,
             self.leave_server_button,
-            self.test_button,
-            self.launch_button,
-            self.add_bench_1_button,
-            self.add_bench_2_button,
-            self.add_played_1_button,
-            self.add_played_2_button,
-            self.reset_button,
+            self.ready_button,
+            self.player_name_text_field,
         ]
 
     def draw(self, window: pygame.Surface):
