5549171867fbf35762fd66a452b69daeb3341576
Marien Giordani <giordanimarien@gmail.com>
2024-09-07 01:15:04 +0200

    Made menu screen inherits from interface to simplify the way it works.

--- src/challengers/client/gui/menu_screen.py
+++ src/challengers/client/gui/menu_screen.py
@@ -1,23 +1,22 @@
-import pygame
+from .components import (
+    Button,
+    TextField,
+    Interface,
+)
 
 
-from .components import Button, TextField
-
-
-class MenuScreen:
+class MenuScreen(Interface):
     def __init__(self):
-        self.enter_server_button: Button = Button(10, 10, "Enter server")
-        self.leave_server_button: Button = Button(300, 10, "leave server")
-        self.ready_button: Button = Button(10, 300, "Ready")
-        self.player_name_text_field: TextField = TextField(300, 300, "Type your name here")
+        self.enter_server_button = Button(10, 10, "Enter server")
+        self.leave_server_button = Button(300, 10, "leave server")
+        self.ready_button = Button(10, 300, "Ready")
+        self.player_name_text_field = TextField(300, 300, "Type your name here")
 
-        self.buttons: list[Button] = [
-            self.enter_server_button,
-            self.leave_server_button,
-            self.ready_button,
-            self.player_name_text_field,
-        ]
-
-    def draw(self, window: pygame.Surface):
-        for button in self.buttons:
-            button.draw(window)
+        super().__init__(
+            [
+                self.enter_server_button,
+                self.leave_server_button,
+                self.ready_button,
+                self.player_name_text_field,
+            ]
+        )
