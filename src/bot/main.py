b3ce28a7bf3167337c34b69855a7c27224c3ffe5
Marien Giordani <giordanimarien@gmail.com>
2024-09-20 00:09:10 +0200

    Addition of test code to test the tournament with the discord bot.

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,6 +1,9 @@
 import hikari
 from pathlib import Path
 
+from challengers.game import Tournament, Player
+from challengers.main import CARD_DATA_FILE_PATH, TROPHY_DATA_FILE_PATH
+
 
 TOKEN_FILE = Path(__file__).parent / "token"
 
@@ -11,12 +14,41 @@ file.close()
 
 bot = hikari.GatewayBot(token)
 
+started = False
+player = Player(0, "TestP")
+
+
+async def main():
+    tournament = Tournament(1)
+    tournament.load_game_cards(CARD_DATA_FILE_PATH)
+    tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
+
+    tournament.set_new_player(player)
+
+    winner = await tournament.play()
+
+    if winner:
+        print("Winner is " + winner)
+
+        tournament.print_scores()
+
 
 @bot.listen()
 async def ping(event: hikari.GuildMessageCreateEvent) -> None:
+    global started
+    global player
     if event.is_human:
         if bot.get_me().id in event.message.user_mentions_ids:
             await event.message.respond(event.author.mention)
+
+            if not started:
+                started = True
+                await main()
+            else:
+                if player.has_to_manage_cards:
+                    player.done_managing_cards()
+                if player.has_to_play:
+                    player.play()
 
 
 bot.run()
