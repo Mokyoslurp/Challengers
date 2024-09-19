4bebb75db16cd1fdd44436588efee9e21088ac27
Marien Giordani <giordanimarien@gmail.com>
2024-09-20 00:04:52 +0200

    Addition of basic test code for the tournament.

--- src/challengers/main.py
+++ src/challengers/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
+import asyncio
 
-from challengers.game import Tournament
+from challengers.game import Tournament, Player
 from challengers.server import Server
 
 
@@ -13,13 +14,30 @@ CARD_DATA_FILE_PATH = GAME_DATA_PATH / CARD_DATA_FILE
 TROPHY_DATA_FILE_PATH = GAME_DATA_PATH / TROPHY_DATA_FILE
 
 
-if __name__ == "__main__":
+async def main():
+    tournament = Tournament(1)
+    tournament.load_game_cards(CARD_DATA_FILE_PATH)
+    tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
+
+    player = Player(0, "TestP")
+    tournament.set_new_player(player)
+
+    winner = await tournament.play()
+
+    if winner:
+        print("Winner is " + winner)
+
+        tournament.print_scores()
+
+
+def server_main(self):
     server = Server(Tournament(1))
 
     server.tournament.load_game_cards(CARD_DATA_FILE_PATH)
     server.tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
 
     server.run()
-    # winner = tournament.play()
 
-    # tournament.print_scores()
+
+if __name__ == "__main__":
+    asyncio.run(main())
