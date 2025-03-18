e140409d73f1707fe5baf06fc43d5e8df30e94bc
Marien Giordani <giordanimarien@gmail.com>
2025-03-18 21:12:02 +0100

    Added configuration of tournament before launching the server.

--- src/challengers/server/server.py
+++ src/challengers/server/server.py
@@ -1,6 +1,7 @@
 import socket as s
 import pickle
 import threading
+from pathlib import Path
 
 from challengers.game import Tournament, Player, Level, TournamentPlan, CardList, Card
 
@@ -270,5 +271,16 @@ class Server:
 
 
 if __name__ == "__main__":
-    server = Server()
+    GAME_DATA_PATH = Path(__file__).parent.parent / "game" / "data"
+    CARD_DATA_FILE = "cards.json"
+    TROPHY_DATA_FILE = "trophies.json"
+
+    CARD_DATA_FILE_PATH = GAME_DATA_PATH / CARD_DATA_FILE
+    TROPHY_DATA_FILE_PATH = GAME_DATA_PATH / TROPHY_DATA_FILE
+
+    tournament = Tournament(1)
+    tournament.load_game_cards(CARD_DATA_FILE_PATH)
+    tournament.load_game_trophies(TROPHY_DATA_FILE_PATH)
+
+    server = Server(tournament)
     server.run()
